8b9a3da781334521664519fd4bc30b268868270d
Deluxan Mariathasan <deluxan.mariathasan@gmail.com>
2024-03-30 18:13:46 +0530

    lint

--- src/test-utils/test-wrapper.tsx
+++ src/test-utils/test-wrapper.tsx
@@ -1,11 +1,9 @@
 import App from 'App';
-import ScrollTop from 'components/ScrollTop';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { store } from 'store';
-import ThemeCustomization from 'themes';
 
-const TestWrapper = ({ children }) => {
+const TestWrapper = () => {
   return (
     <Provider store={store}>
       <BrowserRouter>
