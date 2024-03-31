873f38eefbf8d9e8733930b83fc89efef08717db
Deluxan Mariathasan <deluxan.mariathasan@gmail.com>
2024-03-31 10:17:12 +0530

    logo

--- src/layout/MainLayout/Drawer/DrawerHeader/index.js
+++ src/layout/MainLayout/Drawer/DrawerHeader/index.js
@@ -14,7 +14,6 @@ const DrawerHeader = ({ open }) => {
   const theme = useTheme();
 
   return (
-    // only available in paid version
     <DrawerHeaderStyled theme={theme} open={open}>
       <Stack direction="row" spacing={1} alignItems="center">
         <Logo />
